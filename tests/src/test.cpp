94669e0c0608ddb81c39732eb0f4515486c1bebf
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-22 19:31:47 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,38 +1,41 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/cf/array.hpp>
 
-TEST_CASE("make_empty_cf_array") {
-  auto array = pqrs::cf::make_empty_cf_array();
-  REQUIRE(array);
-  REQUIRE(CFArrayGetCount(*array) == 0);
-}
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
 
-TEST_CASE("make_cf_mutable_array, get_cf_array_value") {
-  {
-    auto array = pqrs::cf::make_cf_mutable_array();
-    REQUIRE(array);
-    REQUIRE(CFGetRetainCount(*array) == 1);
-    REQUIRE(CFArrayGetCount(*array) == 0);
+  "make_empty_cf_array"_test = [] {
+    auto array = pqrs::cf::make_empty_cf_array();
+    expect(array);
+    expect(CFArrayGetCount(*array) == 0);
+  };
 
-    auto string1 = CFSTR("string1");
-    auto string2 = CFSTR("string2");
+  "make_cf_mutable_array, get_cf_array_value"_test = [] {
+    {
+      auto array = pqrs::cf::make_cf_mutable_array();
+      expect(array);
+      expect(CFGetRetainCount(*array) == 1);
+      expect(CFArrayGetCount(*array) == 0);
 
-    CFArrayAppendValue(*array, string1);
-    CFArrayAppendValue(*array, string2);
+      auto string1 = CFSTR("string1");
+      auto string2 = CFSTR("string2");
 
-    REQUIRE(CFArrayGetCount(*array) == 2);
+      CFArrayAppendValue(*array, string1);
+      CFArrayAppendValue(*array, string2);
 
-    auto value1 = pqrs::cf::get_cf_array_value<CFStringRef>(*array, 0);
-    REQUIRE(value1);
-    REQUIRE(CFEqual(string1, value1));
+      expect(CFArrayGetCount(*array) == 2);
 
-    auto value2 = pqrs::cf::get_cf_array_value<CFStringRef>(*array, 1);
-    REQUIRE(value2);
-    REQUIRE(CFEqual(string2, value2));
+      auto value1 = pqrs::cf::get_cf_array_value<CFStringRef>(*array, 0);
+      expect(value1);
+      expect(CFEqual(string1, value1));
 
-    auto value3 = pqrs::cf::get_cf_array_value<CFStringRef>(*array, 2);
-    REQUIRE(!value3);
-  }
+      auto value2 = pqrs::cf::get_cf_array_value<CFStringRef>(*array, 1);
+      expect(value2);
+      expect(CFEqual(string2, value2));
+
+      auto value3 = pqrs::cf::get_cf_array_value<CFStringRef>(*array, 2);
+      expect(!value3);
+    }
+  };
 }
